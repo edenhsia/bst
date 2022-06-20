46dcde5dc32a4ab5260ca6698dc54160b6015289
Eden <edenhsia@gmail.com>
2022-06-20 11:47:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-h4">Block studio test</h1>
+  <IconArrow class="text-primary" />
+</template>
+
+<script>
+import IconArrow from './assets/icons/mail.svg'
+
+export default {
+  components: {
+    IconArrow,
+  },
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  safelist: ['fill-current', 'stroke-current'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      primary: {
+        DEFAULT: '#D68F4C',
+        light: '#DFAD77',
+        lighter: '#DFAD771A',
+      },
+      secondary: {
+        DEFAULT: '#DBBB76',
+        light: '#F2EEE6',
+        lighter: '#F0EDE8',
+      },
+      gray: {
+        lighter: '#F2F2F2',
+        light: '#1313131A',
+        DEFAULT: '#1313134D',
+        dark: '#505050',
+        darker: '#131313',
+      },
+    },
+    extend: {
+      fontFamily: {
+        optima: ['optima', ...defaultTheme.fontFamily.serif],
+        'optima-bold': ['optima-bold', ...defaultTheme.fontFamily.serif],
+        inter: ['inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
