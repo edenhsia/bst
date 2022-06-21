d92631e7d60e902481f4b0e54b85f1f5147d0f3b
Eden <edenhsia@gmail.com>
2022-06-22 01:11:06 +0800

    fix collection-card hover style

--- src/components/CollectionCard.vue
+++ src/components/CollectionCard.vue
@@ -21,12 +21,29 @@ export default {
 </script>
 
 <style scoped>
-.image img {
-  @apply absolute w-full h-full left-0 top-0 object-cover;
-}
-.btn {
-  @apply bg-secondary-lighter shadow px-10 py-4 rounded-full whitespace-nowrap;
-  @apply absolute bottom-6 left-1/2 -translate-x-1/2 transition-colors duration-300;
-  @apply hover:bg-secondary hover:text-white active:bg-primary active:shadow-none;
+.collection-card {
+  @apply overflow-hidden;
+  .image img {
+    @apply absolute w-full h-full left-0 top-0 object-cover transition-transform duration-300;
+  }
+  .btn {
+    @apply bg-secondary-lighter shadow px-10 py-4 rounded-full whitespace-nowrap;
+    @apply absolute bottom-6 left-1/2 -translate-x-1/2 transition-colors duration-300;
+  }
+
+  &:hover {
+    @apply cursor-pointer;
+    .image img {
+      @apply scale-110;
+    }
+
+    .btn {
+      @apply bg-secondary text-white;
+    }
+  }
+
+  &:active .btn {
+    @apply bg-primary shadow-none;
+  }
 }
 </style>
